18f6045e49d636bd3aa59b2376a9b354b0f2075a
hiddro <edd.ckalb@gmail.com>
2022-06-10 18:49:06 -0500

    Edu: proyecto inicial clinicachavez

---
+++ src/main/java/com/peru/srv/clinicachavez/ClinicaChavezApplication.java
@@ -0,0 +1,13 @@
+package com.peru.srv.clinicachavez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaChavezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaChavezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peru/srv/clinicachavez/ClinicaChavezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peru.srv.clinicachavez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaChavezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
